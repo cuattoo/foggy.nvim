e226934d6114350a4675c2ac6a25e74bdcbfbc7f
cuattoo <b.timoshz@gmail.com>
2024-03-21 08:51:47 +0100

    foggy

--- lua/lualine/themes/foggy.lua
+++ lua/lualine/themes/foggy.lua
@@ -1,47 +1,47 @@
-local C = require 'arcdic.colors'
+local C = require 'foggy.colors'
 
-local arcdic = {}
+local foggy = {}
 
-arcdic.normal = {
+foggy.normal = {
     a = { bg = C.xterm.white, fg = C.xterm.grey, gui = 'bold' },
     b = { bg = C.gray.g2, fg = C.xterm.grey },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
-arcdic.insert = {
+foggy.insert = {
     a = { bg = C.green.bright, fg = C.black.b0, gui = 'bold' },
     b = { bg = C.gray.g2, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
-arcdic.command = {
+foggy.command = {
     a = { bg = C.cyan.bright, fg = C.black.b0, gui = 'bold' },
     b = { bg = C.gray.g2, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
-arcdic.visual = {
+foggy.visual = {
     a = { bg = C.red.bright, fg = C.black.b0, gui = 'bold' },
     b = { bg = C.gray.g2, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
-arcdic.replace = {
+foggy.replace = {
     a = { bg = C.magenta.bright, fg = C.black.b0, gui = 'bold' },
     b = { bg = C.gray.g2, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
-arcdic.terminal = {
+foggy.terminal = {
     a = { bg = C.blue.b2, fg = C.black.b0, gui = 'bold' },
     b = { bg = C.gray.g2, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
-arcdic.inactive = {
+foggy.inactive = {
     a = { bg = C.bg_t, fg = C.black.b0, gui = 'bold' },
     b = { bg = C.gray.g2, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
-return arcdic
+return foggy
