59224a09f61dc96de3a027a85cdc519f4eda8f2d
cuattoo <b.timoshz@gmail.com>
2024-03-23 18:23:42 +0100

    Change colors

--- lua/lualine/themes/foggy.lua
+++ lua/lualine/themes/foggy.lua
@@ -4,43 +4,43 @@ local foggy = {}
 
 foggy.normal = {
     a = { bg = C.xterm.white, fg = C.xterm.grey, gui = 'bold' },
-    b = { bg = C.gray.g2, fg = C.xterm.grey },
+    b = { bg = C.bg, fg = C.xterm.grey },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
 foggy.insert = {
     a = { bg = C.green.bright, fg = C.black.b0, gui = 'bold' },
-    b = { bg = C.gray.g2, fg = C.white1 },
+    b = { bg = C.bg, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
 foggy.command = {
     a = { bg = C.cyan.bright, fg = C.black.b0, gui = 'bold' },
-    b = { bg = C.gray.g2, fg = C.white1 },
+    b = { bg = C.bg, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
 foggy.visual = {
     a = { bg = C.red.bright, fg = C.black.b0, gui = 'bold' },
-    b = { bg = C.gray.g2, fg = C.white1 },
+    b = { bg = C.bg, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
 foggy.replace = {
     a = { bg = C.magenta.bright, fg = C.black.b0, gui = 'bold' },
-    b = { bg = C.gray.g2, fg = C.white1 },
+    b = { bg = C.bg, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
 foggy.terminal = {
     a = { bg = C.blue.b2, fg = C.black.b0, gui = 'bold' },
-    b = { bg = C.gray.g2, fg = C.white1 },
+    b = { bg = C.bg, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
 foggy.inactive = {
     a = { bg = C.bg_t, fg = C.black.b0, gui = 'bold' },
-    b = { bg = C.gray.g2, fg = C.white1 },
+    b = { bg = C.bg, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white.default },
 }
 
