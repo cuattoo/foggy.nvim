4d5a1ed7eab7d3825ae7893388993e9715f336ae
cuattoo <b.timoshz@gmail.com>
2024-04-04 20:35:04 +0200

    Updated the colors

--- lua/foggy/colors/init.lua
+++ lua/foggy/colors/init.lua
@@ -23,7 +23,7 @@ function C.extend_palette(style)
     -- Some of the format is from @folke/tokyonight.nvim.
 
     -- Backgrounds
-    C.bg = C.gray.g0
+    C.bg = C.black.medium
     C.bg_dark = C.black.dark
     C.bg_inactive = C.black.dark
     C.bg_highlight = C.gray.g1
