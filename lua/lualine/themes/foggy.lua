b4e473d5e279e47a50b4f693558724ccbfad2b17
cuattoo <b.timoshz@gmail.com>
2024-05-26 15:40:03 +0200

    Updated lualine colors

--- lua/lualine/themes/foggy.lua
+++ lua/lualine/themes/foggy.lua
@@ -3,45 +3,45 @@ local C = require 'foggy.colors'
 local foggy = {}
 
 foggy.normal = {
-    a = { bg = C.orange.base, fg = C.black.dark, gui = 'bold' },
-    b = { bg = C.bg, fg = C.gray.dull1 },
-    c = { bg = C.bg_statusline, fg = C.white.normal },
+	a = { bg = C.orange.base, fg = C.black.dark, gui = 'bold' },
+	b = { bg = C.bg, fg = C.white.medium },
+	c = { bg = C.bg_statusline, fg = C.white.medium },
 }
 
 foggy.insert = {
-    a = { bg = C.green.bright, fg = C.black.dark, gui = 'bold' },
-    b = { bg = C.bg, fg = C.white.normal },
-    c = { bg = C.bg_statusline, fg = C.white.normal },
+	a = { bg = C.green.bright, fg = C.black.dark, gui = 'bold' },
+	b = { bg = C.bg, fg = C.white.medium },
+	c = { bg = C.bg_statusline, fg = C.white.medium },
 }
 
 foggy.command = {
-    a = { bg = C.cyan.bright, fg = C.black.dark, gui = 'bold' },
-    b = { bg = C.bg, fg = C.white.normal },
-    c = { bg = C.bg_statusline, fg = C.white.normal },
+	a = { bg = C.cyan.bright, fg = C.black.dark, gui = 'bold' },
+	b = { bg = C.bg, fg = C.white.medium },
+	c = { bg = C.bg_statusline, fg = C.white.medium },
 }
 
 foggy.visual = {
-    a = { bg = C.red.bright, fg = C.black.dark, gui = 'bold' },
-    b = { bg = C.bg, fg = C.white.normal },
-    c = { bg = C.bg_statusline, fg = C.white.normal },
+	a = { bg = C.red.bright, fg = C.black.dark, gui = 'bold' },
+	b = { bg = C.bg, fg = C.white.medium },
+	c = { bg = C.bg_statusline, fg = C.white.medium },
 }
 
 foggy.replace = {
-    a = { bg = C.magenta.bright, fg = C.black.dark, gui = 'bold' },
-    b = { bg = C.bg, fg = C.white.normal },
-    c = { bg = C.bg_statusline, fg = C.white.normal },
+	a = { bg = C.magenta.bright, fg = C.black.dark, gui = 'bold' },
+	b = { bg = C.bg, fg = C.white.medium },
+	c = { bg = C.bg_statusline, fg = C.white.medium },
 }
 
 foggy.terminal = {
-    a = { bg = C.blue.b2, fg = C.black.dark, gui = 'bold' },
-    b = { bg = C.bg, fg = C.white.normal },
-    c = { bg = C.bg_statusline, fg = C.white.normal },
+	a = { bg = C.blue.base, fg = C.black.dark, gui = 'bold' },
+	b = { bg = C.bg, fg = C.white.medium },
+	c = { bg = C.bg_statusline, fg = C.white.medium },
 }
 
 foggy.inactive = {
-    a = { bg = C.bg_inactive, fg = C.black.dark, gui = 'bold' },
-    b = { bg = C.bg, fg = C.white.normal },
-    c = { bg = C.bg_statusline, fg = C.white.normal },
+	a = { bg = C.bg_inactive, fg = C.black.dark, gui = 'bold' },
+	b = { bg = C.bg, fg = C.white.medium },
+	c = { bg = C.bg_statusline, fg = C.white.medium },
 }
 
 return foggy
