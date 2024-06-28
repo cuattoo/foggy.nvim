9dbaff7452244a4374c5cc8979dc3d4948e14a1d
cuattoo <timomix13@gmail.com>
2024-06-28 16:14:39 +0200

    Updated colors

--- lua/ImperialIllumination/groups/init.lua
+++ lua/ImperialIllumination/groups/init.lua
@@ -25,6 +25,7 @@ M.integrations = {
     'lazy',
     'lsp_signature',
     'ministatusline',
+    'fzf-lua',
 }
 
 M.native = {
@@ -32,7 +33,6 @@ M.native = {
     'syntax',
     'diff',
     'lsp',
-    'terminal',
 }
 
 M.styles = {
